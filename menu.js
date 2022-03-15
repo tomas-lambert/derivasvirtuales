45979e8a71c6a37cdccaf766adf458cfc9bad926
tomas-lambert <lambertomas@gmail.com>
2022-03-15 16:17:14 -0300

    Menu desplegable terminado
    Menu desplegable nuevo, unidades relativas (sin Responsive)

--- menu.js
+++ menu.js
@@ -180,57 +180,23 @@ function createMenu() {
       <img src="./assets/img/iconos/cerrar.svg" onclick="closeMenu()"/>
     </div>
     <div class="body">
-      <div class="recorridos">
-        <div class="submenu-header" onclick="handleSubMenu('recorridos')">
-          <div>
-            <div class="recorridos-line line-top"></div>
-            <p class="submenu-title title-recorridos">RECORRIDOS URBANOS</p>
-          </div>
-          <img src="./assets/img/iconos/desplegable-verde.svg"/>
-        </div>
-        <ul class="list-menu close-menu" id="recorridos">
-          ${listRecorridos.map(item => `
-            <li onclick="onSwitch('${item.type}', 'verde')" class="switch">
-              ${item.template}
-            </li>`).join('')}
-        </ul>
-      </div>
-      <div class="espacios margin-menu">
-        <div class="submenu-header" onclick="handleSubMenu('espacios')">
-          <div>
-            <div class="espacios-line line-top"></div>
-            <p class="submenu-title title-espacios">ESPACIOS DE ARTE</p>
-          </div>
-          <img src="./assets/img/iconos/desplegable-amarillo.svg"/>
-        </div>
-        <ul class="list-menu close-menu" id="espacios">
-          ${listEspacios.map(item => `
-            <li onclick="onSwitch('${item.type}', 'amarillo')" class="switch">
-              ${item.template}
-            </li>`).join('')}
-        </ul>
-      </div>
-      <div class="obras margin-menu">
-        <div class="submenu-header" onclick="handleSubMenu('obras')">
-          <div>
-            <div class="obras-line line-top"></div>
-            <p class="submenu-title title-obras">OBRAS Y EXPOSICIONES</p>
-          </div>
-          <img src="./assets/img/iconos/desplegable-rosa.svg"/>
-        </div>
-        <ul id="obras" class="list-menu close-menu">
-          ${listObras.map(item => `
-            <li onclick="onSwitch('${item.type}', 'rosa')" class="switch">
-              ${item.template}
-            </li>`).join('')}
-        </ul>
+      <div class="accessLink">
+        <img src="./assets/img/iconos/menu-final.svg"/>
+        <a class="item-link" href="./acercade.html">Acerca de Derivas Virtuales</a>
+        <a class="item-link" href="./navegacion.html">Mapa de Navegación</a>
+        <a class="item-link" href="https://drive.google.com/file/d/1QnO-wGAJE5Ypdm3wzGbDxyMZue0J71Qx/view" >Publicación</a>
       </div>
     </div>
     <div class="footer">
-      <img src="./assets/img/iconos/menu-final.svg"/>
-      <a class="item-link" href="./acercade.html">ACERCA DE DERIVAS VIRTUALES</a>
-      <a class="item-link" href="./navegacion.html">MAPA DE NAVEGACIÓN</a>
-      <a class="item-link" href="https://drive.google.com/file/d/1QnO-wGAJE5Ypdm3wzGbDxyMZue0J71Qx/view" >PUBLICACIÓN</a>
+        <img src="./assets/img/iconos/menu-final.svg"/>
+        <p> Derivas Virtuales es parte del proyecto de investigación <br>
+            UBATIC de Cátedra La Ferla, 2021-2022.</p>
+        <div class="ig-link">
+          <a class="ig-container"  href="#" >
+            <img src="./assets/icons/ig-logo.png" alt="Instagram">
+            <h3>@derivasvirtuales</h3>
+          </a>
+        </div>
     </div>
   `;
 }
